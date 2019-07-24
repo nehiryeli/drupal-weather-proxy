7a373a44fb1b7a3ddadc8429eb5d08254238de8d
Hakan Tatlı <hakantatli14@gmail.com>
2019-07-24 14:37:14 +0300

    Weather Proxy for dark sky api

---
+++ src/Controller/ProxyController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Drupal\weather_proxy\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * An example controller.
+ */
+class ProxyController extends ControllerBase {
+
+  /**
+   * Returns a render-able array for a test page.
+   */
+  public function content() {
+  	$url = "https://api.darksky.net/forecast/c9124621e94d5d51beaec54e77f5123d/41.015137,%2028.979530/?units=si&exclude=minutely,hourly,alerts,flags";
+  	header("Access-Control-Allow-Origin: *");
+  	//$data = file_get_contents($url);
+
+  	$response = new Response();
+  	$response->setContent(file_get_contents($url));
+  	$response->headers->set('Content-Type', 'application/json');
+  	return $response;
+  }
+
+
+  
+
+}
