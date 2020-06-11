2f811a24e0da5adb4a053fd92dde64891f727a19
nehiryeli <hakantatli14@gmail.com>
2020-06-11 15:28:18 +0300

    initial commit

--- src/Controller/ProxyController.php
+++ src/Controller/ProxyController.php
@@ -14,11 +14,17 @@ class ProxyController extends ControllerBase {
    */
   public function content() {
   	$url = "https://api.darksky.net/forecast/c9124621e94d5d51beaec54e77f5123d/41.015137,%2028.979530/?units=si&exclude=minutely,hourly,alerts,flags";
-  	header("Access-Control-Allow-Origin: *");
+  	//header("Access-Control-Allow-Origin: *");
   	//$data = file_get_contents($url);
 
   	$response = new Response();
-  	$response->setContent(file_get_contents($url));
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+    {
+      $response->setContent(file_get_contents($url));
+    }else{
+      $response->setContent("Error");
+    }
+  	
   	$response->headers->set('Content-Type', 'application/json');
   	return $response;
   }
